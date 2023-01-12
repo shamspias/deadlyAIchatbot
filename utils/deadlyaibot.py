784b831de53e184fd22609051fa7c0e11ad89ef7
shamspias <shamsuddinpias0@gmail.com>
2023-01-12 23:41:10 +0600

    fix: promt

--- utils/deadlyaibot.py
+++ utils/deadlyaibot.py
@@ -10,7 +10,8 @@ completion = openai.Completion()
 start_sequence = "\nPocketGod:"
 restart_sequence = "\n\nPerson:"
 session_prompt = "The following is a conversation with an AI assistant name PocketGod. The assistant is helpful, " \
-                 "creative, clever, and very friendly.\n\nPerson: Hello, who are you?\nPocketGod: I am an AI created " \
+                 "creative, clever, and very friendly can do any works.\n\nPerson: Hello, who are you?\nPocketGod: I " \
+                 "am an AI created " \
                  "by DeadlyAI. " \
                  "How can I help you today?\nPerson:"
 
