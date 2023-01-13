27d9b087829e337120b5d35bf04734bd8c3c64df
shamspias <shamsuddinpias0@gmail.com>
2023-01-13 21:14:54 +0600

    fix: celery issue

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, request, session, jsonify
-from celery_worker import app, celery
+
+from celery import Celery
+
+app = Flask(__name__)
+app.config.update(CELERY_CONFIG={
+    'broker_url': 'redis://localhost:6379',
+    'result_backend': 'redis://localhost:6379',
+})
+
+
+def make_celery(flask_app):
+    celery = Celery(
+        flask_app.import_name,
+        result_backend=flask_app.config["RESULT_BACKEND_CELERY"],
+        broker=flask_app.config["CELERY_BROKER_URL"],
+    )
+    celery.conf.update(flask_app.config)
+
+    return celery
+
+
+celery = make_celery(app)
 
 from utils.deadlyaibot import get_answer, append_interaction_to_chat_log
 from app.whatsapp_client import WhatsAppWrapper
