9e4bc2728b7f308dd61bccd4d9db09655b98ed35
shamspias <shamsuddinpias0@gmail.com>
2023-01-16 00:01:20 +0600

    refactor: path

--- src/blueprints/pages/views.py
+++ src/blueprints/pages/views.py
@@ -4,8 +4,6 @@ from flask import (Blueprint,
                    jsonify,
                    session)
 from config.settings import WHATSAPP_HOOK_TOKEN
-from open_ai_connection import ask, append_interaction_to_chat_log
-from whatsapp_client import WhatsAppWrapper
 
 page = Blueprint('pages', __name__, template_folder='templates')
 
@@ -35,3 +33,79 @@ def _execute_task():
         task = my_task.delay()
 
     return jsonify({'taskID': task.id}), 201
+
+
+@page.route("/send_template_message/", methods=["POST"])
+def send_template_message():
+    """_summary_: Send a message with a template to a phone number"""
+
+    from open_ai_connection import ask, append_interaction_to_chat_log
+    from src.blueprints.pages.whatsapp_client import WhatsAppWrapper
+
+    if "language_code" not in request.json:
+        return jsonify({"error": "Missing language_code"}), 400
+
+    if "phone_number" not in request.json:
+        return jsonify({"error": "Missing phone_number"}), 400
+
+    if "template_name" not in request.json:
+        return jsonify({"error": "Missing template_name"}), 400
+
+    client = WhatsAppWrapper()
+
+    response = client.send_template_message(
+        template_name=request.json["template_name"],
+        language_code=request.json["language_code"],
+        phone_number=request.json["phone_number"],
+    )
+    print(response)
+    return jsonify(
+        {
+            "data": response,
+            "status": "success",
+        },
+    ), 200
+
+
+@page.route('/deadlyai/', methods=["POST", "GET"])
+def deadly_text_chat():
+    """
+    Send mgs
+    :return:
+    """
+    from src.blueprints.pages.open_ai_connection import ask, append_interaction_to_chat_log
+    from src.blueprints.pages.whatsapp_client import WhatsAppWrapper
+
+    if request.method == "GET":
+        if request.args.get('hub.verify_token') == VERIFY_TOKEN:
+            return request.args.get('hub.challenge')
+        return "Authentication failed. Invalid Token."
+
+    if request.method == "POST":
+        client = WhatsAppWrapper()
+        row_data = request.get_json()
+        data = client.process_webhook_notification(row_data)
+        user = data[0]['from']
+        incoming_msg = data[0]['mgs']
+
+        if incoming_msg is not None:
+            if incoming_msg.startswith("/image"):
+                response = client.send_normal_message.delay("Image will be added soon", user)
+            else:
+                chat_log = session.get('chat_log')
+                answer = ask.delay(incoming_msg, chat_log)
+                session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
+                                                                     chat_log)
+                if user is not None:
+                    response = client.send_normal_message.delay(answer, user)
+                else:
+                    response = "No User"
+        else:
+            response = "No Data"
+
+        return jsonify(
+            {
+                "data": response,
+                "status": "success",
+            },
+        ), 200
