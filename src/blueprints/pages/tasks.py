295e6c59854f9e65de8d4513af8718b53cebdec3
shamspias <shamsuddinpias0@gmail.com>
2023-01-15 22:48:06 +0600

    refactor: celery

--- src/blueprints/pages/tasks.py
+++ src/blueprints/pages/tasks.py
@@ -6,29 +6,6 @@ celery = celery_app
 
 @celery.task(bind=True)
 def my_task(self):
-    choice = random.choice(['Alpha',
-                            'Beta',
-                            'Gamma',
-                            'Delta',
-                            'Epsilon',
-                            'Zeta',
-                            'Eta',
-                            'Theta',
-                            'Iota',
-                            'Kappa',
-                            'Lambda',
-                            'Mu',
-                            'Nu',
-                            'Xi',
-                            'Omicron',
-                            'Pi',
-                            'Rho',
-                            'Sigma',
-                            'Tau',
-                            'Upsilon',
-                            'Phi',
-                            'Chi',
-                            'Psi',
-                            'Omega'])
+    choice = random.choice(['Psi', 'Omega'])
 
     return choice
