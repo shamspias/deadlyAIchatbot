8e879f216382328825fd931ab1959abf28f88d98
shamspias <shamsuddinpias0@gmail.com>
2023-01-15 23:47:06 +0600

    refactor: openai

--- src/celery_task_registry.py
+++ src/celery_task_registry.py
@@ -9,4 +9,5 @@ celery = Celery(app.import_name,
 CELERY_TASK_LIST = [
     'src.blueprints.pages.tasks',
     'src.blueprints.pages.whatsapp_client',
+    'src.blueprints.pages.open_ai_connection',
 ]
