3a7b2ae9f62329980b09519f6ef96c5d0b365d86
shamspias <shamsuddinpias0@gmail.com>
2023-01-16 00:11:38 +0600

    refactor: only api

--- src/blueprints/pages/views.py
+++ src/blueprints/pages/views.py
@@ -10,29 +10,15 @@ page = Blueprint('pages', __name__, template_folder='templates')
 VERIFY_TOKEN = WHATSAPP_HOOK_TOKEN
 
 
-@page.route("/")
-def index():
-    """
-    Index / Main page
-    :return: html
-    """
-
-    return render_template('index.html')
-
-
-@page.route("/_execute_task", methods=['POST'])
-def _execute_task():
-    """
-    Invoke this function from an Ajax Call
-    :return: json
-    """
-    from src.blueprints.pages.tasks import my_task
-
-    if request.method == 'POST':
-        # Invoke celery task
-        task = my_task.delay()
-
-    return jsonify({'taskID': task.id}), 201
+# @page.route("/")
+# def index():
+#     """
+#     Index / Main page
+#     :return: html
+#     """
+#
+#     return render_template('index.html')
+#
 
 
 @page.route("/send_template_message/", methods=["POST"])
